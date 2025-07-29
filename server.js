f12a6f6feb70fe42cfeeed674c97f001ba1b9c1e
Haziq Rozi <mhaziqjr07@gmail.com>
2025-07-29 21:14:53 +0800

    travelog v2

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const PORT = process.env.PORT || 3001;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "http://localhost:5174",
+    ],
     credentials: true,
   })
 );
